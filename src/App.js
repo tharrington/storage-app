2162ceeed8cb604487ccfcd0d46b1bd054d937d9
Tyler Harrington <tyler.harrington1989@gmail.com>
2022-02-11 10:30:12 -0500

    added refs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
+import React, { useState } from 'react';
+
 import Registration from './components/Users/Registration';
+import UsersList from './components/Users/UserList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const registerUser = (fName, lName, email, password) => {
+    console.log('### run');
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, {
+        first_name: fName,
+        last_name: lName,
+        email: email,
+        id: prevUsersList.length + 1
+      }];
+    })
+  };
+
   return (
-    <div className="App">
+    <React.Fragment>
       <header className="App-header">
-        <Registration></Registration>
+        <Registration onRegisterUser={registerUser}></Registration>
+        <UsersList users={usersList}></UsersList>
       </header>
-    </div>
+    </React.Fragment>
   );
 }
 
--- src/components/Users/registration.js
+++ src/components/Users/registration.js
@@ -1,37 +1,64 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
+
 import Card from '../UI/Card';
 import classes from './Registration.module.css';
 import Button from '../UI/Button';
+import ErrorModal from "../UI/ErrorModal";
 
 const Registration = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const fNameInputRef = useRef();
+  const lNameInputRef = useRef();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const [error, setError] = useState('');
 
   const registerUserHandler = (event) => {
     event.preventDefault();
-    console.log(enteredEmail, enteredPassword);
-    setEnteredPassword('');
-    setEnteredEmail('');
-  }
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFName = fNameInputRef.current.value;
+    const enteredLName = lNameInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
 
-  const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    if(enteredEmail.trim().length === 0 || enteredFName.trim().length === 0 || enteredLName.trim().length === 0 || enteredPassword.trim().length <= 3) {
+      setError({
+        title: 'Invalid input',
+        message: 'Fill out all inputs'
+      })
+      return;
+    }
+    props.onRegisterUser(enteredFName, enteredLName, enteredEmail, enteredPassword);
+    fNameInputRef.current.value = '';
+    lNameInputRef.current.value = '';
+    emailInputRef.current.value = '';
+    passwordInputRef.current.value = '';
   }
-  const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+  const errorHandler = () => {
+    setError(null);
   }
 
   return (
-    <Card className={classes.input}>
-      <form onSubmit={registerUserHandler}>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" onChange={emailChangeHandler}></input>
+    <React.Fragment>
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}></ErrorModal>}
+      <Card className={classes.input}>
+        <form onSubmit={registerUserHandler}>
+          <label htmlFor="first_name">First Name</label>
+          <input id="first_name" type="text" ref={fNameInputRef}></input>
 
-        <label htmlFor="password">Password</label>
-        <input id="password" type="password" onChange={passwordChangeHandler}></input>
-        <Button type="submit">Register</Button>
-      </form>
-    </Card>
+          <label htmlFor="last_name">Last Name</label>
+          <input id="last_name" type="text" ref={lNameInputRef}></input>
+
+          <label htmlFor="email">Email</label>
+          <input id="email" type="text" ref={emailInputRef}></input>
+
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" ref={passwordInputRef}></input>
+
+          <Button type="submit">Register</Button>
+        </form>
+      </Card>
+    </React.Fragment>
+
   );
 }
 
